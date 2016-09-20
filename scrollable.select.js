30368a8e5f0c4c4fa260901bbf900f1e5bbe00d4
Henrique Borba Behr <henriqb@gmail.com>
2016-09-20 14:36:49 +0100

    Create scrollable.select.js
    Src file

---
+++ scrollable.select.js
@@ -0,0 +1,22 @@
+(function () {
+    $(document).ready(function () {
+        $('select').on('mousewheel', function (e) {
+            var select = $(this);
+            var options = select.find('option');
+            var selected;
+            for (var i = 0; i < options.length; i++) {
+                selected = options.eq(i);
+                if (selected.is(':selected')) { break; }
+            }
+            if (e.originalEvent.wheelDelta > 0)
+            {
+                selected.prev().prop('selected', true);
+
+            } else {
+                selected.next().prop('selected', true);
+            }
+            e.preventDefault ? e.preventDefault() : e.returnValue = false;
+            return false;
+        });
+    });
+})();
